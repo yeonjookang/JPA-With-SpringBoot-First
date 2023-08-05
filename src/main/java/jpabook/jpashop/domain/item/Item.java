186ad89ecc87db6def553aca980a694be99b8ded
kang yeon joo <rkdduswn0409@gmail.com>
2023-08-05 14:26:36 +0900

    feat : 카테고리 엔티티 추가 및 아이템 엔티티  수정

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jdk.jfr.Category;
 import jdk.jfr.Enabled;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -24,4 +28,6 @@ public abstract class Item {
     @Column(name="STOCKQUANTITY")
     private int stockQuantity;
 
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
